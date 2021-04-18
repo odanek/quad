4a629da5e599f906b23096c2e177ab2bfcc99773
odanek <ondrej.danek@gmail.com>
2021-04-18 16:43:41 +0200

    Don't export whole modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 mod quad;
-pub mod scene;
-pub mod window;
-pub mod world;
+mod scene;
+mod window;
+mod world;
 
 pub use quad::Quad;
+pub use scene::{Scene, SceneResult};
+pub use window::{Window, WindowBuilder};
+pub use world::{World};
