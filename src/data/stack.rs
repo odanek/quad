fc1972f82f7a8004dc871ed31e9891dc349df2b7
odanek <ondrej.danek@gmail.com>
2021-04-28 14:00:16 +0200

    Fix clippy warning

--- src/data/stack.rs
+++ src/data/stack.rs
@@ -1,5 +1,11 @@
 pub struct Stack<T>(Vec<T>);
 
+impl<T> Default for Stack<T> {
+    fn default() -> Self {
+        Self(Default::default())
+    }
+}
+
 impl<T> Stack<T> {
     pub fn new() -> Stack<T> {
         Self(Vec::new())
@@ -11,6 +17,10 @@ impl<T> Stack<T> {
 
     pub fn pop(&mut self) -> Option<T> {
         self.0.pop()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.0.is_empty()
     }
 
     pub fn len(&self) -> usize {
