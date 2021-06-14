7d796afdae9b60be8a25c33cdc5779c71b743999
odanek <ondrej.danek@gmail.com>
2021-06-14 18:24:41 +0200

    Resource ids

--- src/ecs/resource/mod.rs
+++ src/ecs/resource/mod.rs
@@ -1,54 +1,93 @@
-use std::{
-    any::{Any, TypeId},
-    collections::HashMap,
-};
+use std::{any::{Any, TypeId, type_name}, collections::HashMap};
 
 pub trait Resource: Send + Sync + 'static {}
 impl<T: Send + Sync + 'static> Resource for T {}
 
-// TODO: ResourceId
+#[derive(Debug, Copy, Clone, Hash, Ord, PartialOrd, Eq, PartialEq)]
+pub struct ResourceId(usize);
+
+#[derive(Debug)]
+pub struct ResourceInfo {
+    id: ResourceId,
+    type_id: TypeId,
+    name: &'static str,
+}
+
+impl ResourceInfo {
+    #[inline]
+    pub fn id(&self) -> ResourceId {
+        self.id
+    }
+
+    #[inline]
+    pub fn name(&self) -> &'static str {
+        self.name
+    }
+
+    #[inline]
+    pub fn type_id(&self) -> TypeId {
+        self.type_id
+    }
+}
 
 #[derive(Default)]
 pub struct Resources {
-    map: HashMap<TypeId, Box<dyn Any>>,
+    resources: Vec<ResourceInfo>,
+    id_map: HashMap<TypeId, ResourceId>,
+    map: HashMap<ResourceId, Box<dyn Any>>,
 }
 
 impl Resources {
     #[inline]
-    pub fn add<T: Resource>(&mut self, resource: T) {
-        self.map.insert(resource.type_id(), Box::new(resource));
-        // .expect_none("Resource already exists"); // TODO
+    pub fn get_id<T: Resource>(&self) -> Option<ResourceId> {
+        self.id_map.get(&TypeId::of()).copied()
+    }
+
+    fn insert_id<T: Resource>(&mut self) -> ResourceId {
+        let type_id = TypeId::of::<T>();
+        let id = ResourceId(self.resources.len());
+        let info = ResourceInfo {
+            id,
+            type_id,
+            name: type_name::<T>()
+        };
+        self.resources.push(info);
+        self.id_map.insert(type_id, id); // .expect_none("Resource already exists"); // TODO
+        id
+    }
+
+    #[inline]
+    pub fn add<T: Resource>(&mut self, resource: T) -> ResourceId {
+        let id = self.insert_id::<T>();
+        self.map.insert(id, Box::new(resource));
+        id        
     }
 
     #[inline]
     pub fn remove<T: Resource>(&mut self) -> Option<T> {
-        let type_id = TypeId::of::<T>();
-        self.map.remove(&type_id)?.downcast().ok().map(|v| *v)
+        let id = self.get_id::<T>()?;
+        self.map.remove(&id)?.downcast().ok().map(|v| *v)
     }
 
     #[inline]
     pub fn get<T: Resource>(&self) -> Option<&T> {
-        let type_id = TypeId::of::<T>();
-        self.map.get(&type_id)?.downcast_ref()
+        let id = self.get_id::<T>()?;
+        self.map.get(&id)?.downcast_ref()
     }
 
     #[inline]
     pub unsafe fn get_unchecked<T: Resource>(&self) -> Option<*const T> {
-        let type_id = TypeId::of::<T>();
-        let reference = self.map.get(&type_id)?.downcast_mut()?;
-        Some(reference as _)
+        self.get::<T>().map(|r| r as _)
     }
 
     #[inline]
     pub fn get_mut<T: Resource>(&mut self) -> Option<&mut T> {
-        let type_id = TypeId::of::<T>();
-        self.map.get_mut(&type_id)?.downcast_mut()
+        let id = self.get_id::<T>()?;
+        self.map.get_mut(&id)?.downcast_mut()
     }
 
     #[inline]
     pub unsafe fn get_unchecked_mut<T: Resource>(&self) -> Option<*mut T> {
-        let type_id = TypeId::of::<T>();
-        let reference = self.map.get_mut(&type_id)?.downcast_mut()?;
-        unsafe { Some(reference as _) }
+        self.get_mut::<T>().map(|r| r as _)
     }
 }
