dbae3f44e840b9f7c2caff626689d54dd3718a71
odanek <ondrej.danek@gmail.com>
2021-07-13 15:58:27 +0200

    New method for query iterator

--- src/ecs/query/iter.rs
+++ src/ecs/query/iter.rs
@@ -1,8 +1,14 @@
-use std::slice::Iter;
+use crate::ecs::{
+    entity::archetype::{ArchetypeId, Archetypes},
+    storage::{TableId, Tables},
+    World,
+};
 
-use crate::ecs::{World, entity::archetype::{ArchetypeId, Archetypes}, storage::{TableId, Tables}};
-
-use super::{fetch::WorldQuery, filter::FilterFetch, state::QueryState};
+use super::{
+    fetch::{Fetch, WorldQuery},
+    filter::FilterFetch,
+    state::QueryState,
+};
 
 pub struct QueryIter<'w, 's, Q: WorldQuery, F: WorldQuery>
 where
@@ -12,11 +18,35 @@ where
     archetypes: &'w Archetypes,
     query_state: &'s QueryState<Q, F>,
     world: &'w World,
-    table_id_iter: Iter<'s, TableId>,
-    archetype_id_iter: Iter<'s, ArchetypeId>,
+    table_id_iter: std::collections::hash_set::Iter<'s, TableId>,
+    archetype_id_iter: std::collections::hash_set::Iter<'s, ArchetypeId>,
     fetch: Q::Fetch,
     filter: F::Fetch,
     current_len: usize,
     current_index: usize,
     is_dense: bool,
+}
+
+impl<'w, 's, Q: WorldQuery, F: WorldQuery> QueryIter<'w, 's, Q, F>
+where
+    F::Fetch: FilterFetch,
+{
+    pub(crate) unsafe fn new(world: &'w World, query_state: &'s QueryState<Q, F>) -> Self {
+        let fetch = <Q::Fetch as Fetch>::new(world, &query_state.fetch_state);
+        let filter = <F::Fetch as Fetch>::new(world, &query_state.filter_state);
+
+        QueryIter {
+            world,
+            query_state,
+            tables: &world.storages().tables,
+            archetypes: world.archetypes(),
+            is_dense: fetch.is_dense() && filter.is_dense(),
+            fetch,
+            filter,
+            table_id_iter: query_state.matched_tables.iter(),
+            archetype_id_iter: query_state.matched_archetypes.iter(),
+            current_len: 0,
+            current_index: 0,
+        }
+    }
 }
