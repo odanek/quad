321f2866cfad0864d806ae95fc6c5c56fd0fc45f
odanek <ondrej.danek@gmail.com>
2021-08-02 18:02:03 +0200

    Implement reserve entity

--- src/ecs/entity/mod.rs
+++ src/ecs/entity/mod.rs
@@ -1,3 +1,6 @@
+use std::sync::atomic::{AtomicI64, Ordering};
+use std::convert::TryFrom;
+
 use self::archetype::ArchetypeId;
 
 pub mod archetype;
@@ -28,8 +31,8 @@ impl Entity {
 #[derive(Default)]
 pub struct Entities {
     meta: Vec<EntityMeta>,
-    free: Vec<u32>, // TODO: use the same pattern as in Arena or Bevy
-    len: u32,
+    pending: Vec<u32>,
+    free_cursor: AtomicI64,
 }
 
 #[derive(Copy, Clone, Debug)]
@@ -46,8 +49,10 @@ pub struct EntityLocation {
 
 impl Entities {
     pub fn alloc(&mut self, location: EntityLocation) -> Entity {
-        if let Some(id) = self.free.pop() {
-            self.len += 1;
+        self.verify_flushed();
+        
+        if let Some(id) = self.pending.pop() {
+            *self.free_cursor.get_mut() = self.pending.len() as i64;
             let meta = &mut self.meta[id as usize];
             meta.location = location;
 
@@ -56,8 +61,7 @@ impl Entities {
                 id,
             }
         } else {
-            let id = self.len;
-            self.len = self.len.checked_add(1).expect("Too many entities");
+            let id = u32::try_from(self.meta.len()).expect("too many entities");
             let meta = EntityMeta {
                 generation: 0,
                 location,
@@ -68,21 +72,41 @@ impl Entities {
     }
 
     pub fn free(&mut self, entity: Entity) -> Option<EntityLocation> {
+        self.verify_flushed();
+
         let index = entity.id as usize;
         if index >= self.meta.len() {
             return None;
         }
+
         let meta = &mut self.meta[index];
         if meta.generation != entity.generation {
             return None;
         }
         meta.generation += 1;
 
-        let location = meta.location; // TODO: Reset location to some empty value?
+        self.pending.push(entity.id);
 
-        self.free.push(entity.id);
-        self.len -= 1;
-        Some(location)
+        *self.free_cursor.get_mut() = self.pending.len() as i64;
+        Some(meta.location)
+    }
+
+    pub fn reserve_entity(&self) -> Entity {
+        // TODO: This returns Entity that has valid generation but is in fact invalid until flush is called
+        let n = self.free_cursor.fetch_sub(1, Ordering::Relaxed);
+        if n > 0 {
+            let id = self.pending[(n - 1) as usize];
+            Entity {
+                generation: self.meta[id as usize].generation,
+                id,
+            }
+        } else {
+            let id = u32::try_from(self.meta.len() as i64 - n).expect("too many entities");
+            Entity {
+                generation: 0,
+                id,
+            }
+        }
     }
 
     pub fn get(&self, entity: Entity) -> Option<EntityLocation> {
@@ -115,12 +139,63 @@ impl Entities {
             .map_or(true, |meta| meta.generation == entity.generation)
     }
 
-    pub(crate) fn update_location(&mut self, entity: Entity, location: EntityLocation) {
+    pub fn update_location(&mut self, entity: Entity, location: EntityLocation) {
         self.meta[entity.id as usize].location = location;
     }
 
-    pub fn reserve_entity(&self) -> Entity {
-        // TODO
-        todo!("Implement");
+    pub fn flush(&mut self, mut init: impl FnMut(Entity, &mut EntityLocation)) {
+        todo!()
+        // let free_cursor = self.free_cursor.get_mut();
+        // let current_free_cursor = *free_cursor;
+
+        // let new_free_cursor = if current_free_cursor >= 0 {
+        //     current_free_cursor as usize
+        // } else {
+
+
+
+        //     let old_meta_len = self.meta.len();
+        //     let new_meta_len = old_meta_len + (-current_free_cursor as usize);
+
+
+
+        //     self.meta.resize(new_meta_len, EntityMeta::EMPTY);
+        //     self.len += -current_free_cursor as u32;
+        //     for (id, meta) in self.meta.iter_mut().enumerate().skip(old_meta_len) {
+        //         init(
+        //             Entity {
+        //                 id: id as u32,
+        //                 generation: meta.generation,
+        //             },
+        //             &mut meta.location,
+        //         );
+        //     }
+
+        //     *free_cursor = 0;
+        //     0
+        // };
+
+        // self.len += (self.pending.len() - new_free_cursor) as u32;
+        // for id in self.pending.drain(new_free_cursor..) {
+        //     let meta = &mut self.meta[id as usize];
+        //     init(
+        //         Entity {
+        //             id,
+        //             generation: meta.generation,
+        //         },
+        //         &mut meta.location,
+        //     );
+        // }
+    }    
+
+    fn verify_flushed(&mut self) {
+        debug_assert!(
+            !self.needs_flush(),
+            "flush() needs to be called before this operation is legal"
+        );
+    }
+
+    fn needs_flush(&mut self) -> bool {
+        *self.free_cursor.get_mut() != self.pending.len() as i64
     }
 }
