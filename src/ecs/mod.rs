90804626a99a177d6a18a45cadb800d1503d4039
odanek <ondrej.danek@gmail.com>
2021-08-11 18:07:41 +0200

    Convenience methods for optional Schedule

--- src/ecs/mod.rs
+++ src/ecs/mod.rs
@@ -14,7 +14,7 @@ pub use component::Component;
 pub use entity::Entity;
 pub use query::filter::{Or, With, Without};
 pub use resource::Resource;
-pub use schedule::{Schedule, Scheduler};
+pub use schedule::{OptionalSchedule, OptionalScheduleWithInput, Schedule, Scheduler};
 pub use system::{
     command::Commands,
     local::Local,
--- src/ecs/schedule/mod.rs
+++ src/ecs/schedule/mod.rs
@@ -7,7 +7,7 @@ use super::{System, World};
 mod chain_system;
 mod parallel_system;
 
-pub struct Schedule<In, Out> {
+pub struct Schedule<In = (), Out = ()> {
     system: Box<dyn System<In = In, Out = Out>>,
     marker: PhantomData<(In, Out)>,
 }
@@ -45,13 +45,58 @@ where
     }
 }
 
+pub trait OptionalSchedule {
+    type Out: 'static;
+
+    fn run(&mut self, world: &mut World) -> Self::Out;
+    fn run_or(&mut self, default: Self::Out, world: &mut World) -> Self::Out;
+}
+
+impl<Out> OptionalSchedule for Option<Schedule<(), Out>>
+where
+    Out: 'static,
+{
+    type Out = Out;
+
+    fn run(&mut self, world: &mut World) -> Self::Out {
+        self.as_mut().unwrap().run(world)
+    }
+
+    fn run_or(&mut self, default: Self::Out, world: &mut World) -> Self::Out {
+        self.as_mut()
+            .map_or(default, |schedule| schedule.run(world))
+    }
+}
+
+pub trait OptionalScheduleWithInput {
+    type In: 'static;
+    type Out: 'static;
+
+    fn run_with(&mut self, input: Self::In, world: &mut World) -> Self::Out;
+    fn run_with_or(&mut self, default: Self::Out, input: Self::In, world: &mut World) -> Self::Out;
+}
+
+impl<In, Out> OptionalScheduleWithInput for Option<Schedule<In, Out>>
+where
+    In: 'static,
+    Out: 'static,
+{
+    type In = In;
+    type Out = Out;
+
+    fn run_with(&mut self, input: Self::In, world: &mut World) -> Self::Out {
+        self.as_mut().unwrap().run_with(input, world)
+    }
+
+    fn run_with_or(&mut self, default: Self::Out, input: Self::In, world: &mut World) -> Self::Out {
+        self.as_mut()
+            .map_or(default, |schedule| schedule.run_with(input, world))
+    }
+}
+
 pub struct Scheduler {}
 
 impl Scheduler {
-    // pub fn parallel() -> ParallelBuilder {
-    //     ParallelBuilder::default()
-    // }
-
     pub fn single<S, In, Out>(system: S) -> Schedule<In, Out>
     where
         S: System<In = In, Out = Out>,
@@ -64,4 +109,8 @@ impl Scheduler {
     pub fn chain() -> EmptyChainBuilder {
         Default::default()
     }
+
+    // pub fn parallel() -> ParallelBuilder {
+    //     ParallelBuilder::default()
+    // }
 }
