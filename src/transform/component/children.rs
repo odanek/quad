6f17746301a3c651e36f03d0db05d650a19d61bd
odanek <ondrej.danek@gmail.com>
2021-08-14 19:28:48 +0200

    Children component

---
+++ src/transform/component/children.rs
@@ -0,0 +1,22 @@
+use std::ops::Deref;
+
+use crate::ecs::Entity;
+
+#[derive(Default, Clone, Debug)]
+pub struct Children(pub(crate) Vec<Entity>);
+
+impl Children {
+    pub fn with(entity: &[Entity]) -> Self {
+        let mut data = Vec::with_capacity(8);
+        data.extend_from_slice(entity);
+        Self(data)
+    }
+}
+
+impl Deref for Children {
+    type Target = [Entity];
+
+    fn deref(&self) -> &Self::Target {
+        &self.0[..]
+    }
+}
